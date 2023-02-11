59454d9854b200857f0c629d9a0a9b5e78f8f6c4
root <root@localhost.localdomain>
2023-02-11 06:45:29 +0000

    feat: Add fork, pid, and execve implementation to handle command execution

--- main.c
+++ main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
 #include "parser.h"
 #include "utils.h"
+
+#define BUFSIZE 1024
 
 /**
  * main - The entry point of the shell.
@@ -13,17 +17,52 @@ int main(void)
 {
   char *input = NULL;
   size_t len = 0;
-  ssize_t read;
   Command *cmd;
+  char *path;
+  int pid, status;
+  char *dirs;
+  char *saveptr;
+  char *dir;
+  char buf[BUFSIZE];
 
-  while ((read = getline(&input, &len, stdin)) != -1)
+  while (1)
   {
-    input[strcspn(input, "\n")] = '\0';
-    cmd = parse_input(input);
-    if (!cmd)
+    printf("$ ");
+    if (getline(&input, &len, stdin) == -1) 
+    {
+      printf("\n");
       break;
-
-    print_command(cmd);
+    }
+    input[strlen(input) - 1] = '\0';
+    cmd = parse_input(input);
+    if (cmd->argc == 1 && strcmp(cmd->argv[0], "exit") == 0)
+    {
+      break;
+    }
+    
+    pid = fork();
+    if (pid == 0)
+    {
+      path = getenv("PATH");
+      dirs = strdup(path);
+      
+      for (dir = strtok_r(dirs, ":", &saveptr); dir; dir = strtok_r(NULL, ":", &saveptr))
+      {
+        snprintf(buf, BUFSIZE, "%s/%s", dir, cmd->argv[0]);
+        execve(buf, cmd->argv, NULL);
+      }
+      free(dirs);
+      perror("execve");
+      exit(1);
+    }
+    else if (pid > 0)
+    {
+      wait(&status);
+    }
+    else
+    {
+      perror("fork");
+    }
     free_command(cmd);
   }
 
